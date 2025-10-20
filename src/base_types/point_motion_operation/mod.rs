9add9d17c5d4788f6b88ee2e16999e8d16736315
Indigo Curnick <indigo.c@naurt.com>
2025-10-20 20:43:42 +0100

    added point motion operation

---
+++ src/base_types/point_motion_operation/mod.rs
@@ -0,0 +1,6 @@
+mod point_motion_operation;
+
+#[cfg(test)]
+mod tests;
+
+pub use point_motion_operation::PointMotionOperation;
---
+++ src/base_types/point_motion_operation/tests.rs
@@ -0,0 +1,126 @@
+use crate::{
+    ast::parse_wkt,
+    base_types::{
+        CoordinateSystem, Ellipsoid, GeodeticCrs, GeodeticReferenceFrame, LengthUnit,
+        OperationAccuracy, OperationMethod, OperationParameterFile, Order, PointMotionOperation,
+        SourceCrs, SpatialAxis, SpatialCoordinateSystem, StaticGeodeticCrs,
+    },
+    compound_types::{
+        CoordinateReferenceSystem, GeodeticData, ScopeExtentIdentifierRemark, SingleCrs,
+        SpatialUnit, Unit,
+    },
+    enumerations::{AxisDirection, Dimension, OperationParameterWrapper, SpatialCsType},
+    types::WktBaseType,
+};
+
+const EXAMPLE1: &str = r#"
+POINTMOTIONOPERATION["Canada velocity grid v6",
+	SOURCECRS[
+        GEODCRS["Tokyo",
+            DATUM["Tokyo 1918",
+                ELLIPSOID["Bessel 1841",6377397.155,299.1528128,
+                    LENGTHUNIT["metre",1.0]]],
+            CS[Cartesian,3],
+                AXIS["(X)",geocentricX,ORDER[1]],
+                AXIS["(Y)",geocentricY,ORDER[2]],
+                AXIS["(Z)",geocentricZ,ORDER[3]],
+                LENGTHUNIT["metre",1.0]
+        ]
+	],
+	METHOD["Point motion by grid (Canada NTv2_Vel)"],
+	PARAMETERFILE["Point motion velocity grid file","cvg60.cvb"],
+	OPERATIONACCURACY[0.01]
+]
+"#;
+
+#[test]
+fn test_point_motion_operation() {
+    let correct = PointMotionOperation {
+        operation_name: "Canada velocity grid v6".into(),
+        operation_version: None,
+        source_crs: SourceCrs {
+            coordinate_system: CoordinateReferenceSystem::SingleCrs(SingleCrs::GeodeticCrs(
+                GeodeticCrs::StaticGeodeticCrs(StaticGeodeticCrs {
+                    crs_name: "Tokyo".into(),
+                    frame: GeodeticData::GeodeticReferenceFrame(GeodeticReferenceFrame {
+                        datum_name: "Tokyo 1918".into(),
+                        ellipsoid: Ellipsoid {
+                            ellipsoid_name: "Bessel 1841".into(),
+                            semi_major_axis: 6377397.155,
+                            inverse_flattening: 299.1528128,
+                            length_unit: Some(LengthUnit {
+                                unit_name: "metre".into(),
+                                conversion_factor: 1.0,
+                            }),
+                        },
+                        anchor: None,
+                        identifier: None,
+                        prime_meridian: None,
+                    }),
+                    coordinate_system: CoordinateSystem::SpatialCS(SpatialCoordinateSystem {
+                        spatial_cs_type: SpatialCsType::Cartesian,
+                        dimension: Dimension::Three,
+                        identifier: None,
+                        spatial_axis: vec![
+                            SpatialAxis {
+                                axis_name_abbreviation: "(X)".into(),
+                                axis_direction: AxisDirection::GeocentricX,
+                                axis_order: Some(Order(1)),
+                                spatial_unit: None,
+                                identifier: None,
+                            },
+                            SpatialAxis {
+                                axis_name_abbreviation: "(Y)".into(),
+                                axis_direction: AxisDirection::GeocentricY,
+                                axis_order: Some(Order(2)),
+                                spatial_unit: None,
+                                identifier: None,
+                            },
+                            SpatialAxis {
+                                axis_name_abbreviation: "(Z)".into(),
+                                axis_direction: AxisDirection::GeocentricZ,
+                                axis_order: Some(Order(3)),
+                                spatial_unit: None,
+                                identifier: None,
+                            },
+                        ],
+                        cs_unit: Some(Unit::SpatialUnit(SpatialUnit::LengthUnit(LengthUnit {
+                            unit_name: "metre".into(),
+                            conversion_factor: 1.0,
+                        }))),
+                    }),
+                    scope_extent_identifier_remark: ScopeExtentIdentifierRemark {
+                        identifier: None,
+                        remark: None,
+                        usage: None,
+                    },
+                }),
+            )),
+        },
+        operation_method: OperationMethod {
+            operation_method_name: "Point motion by grid (Canada NTv2_Vel)".into(),
+            identifier: None,
+        },
+        operation_parameter_wrapper: Some(vec![OperationParameterWrapper::OperationParameterFile(
+            OperationParameterFile {
+                parameter_name: "Point motion velocity grid file".into(),
+                parameter_file_name: "cvg60.cvb".into(),
+                identifier: None,
+            },
+        )]),
+        operation_accuracy: Some(OperationAccuracy(0.01)),
+        scope_extent_identifier_remark: ScopeExtentIdentifierRemark {
+            usage: None,
+            identifier: None,
+            remark: None,
+        },
+    };
+
+    let ast = parse_wkt(EXAMPLE1);
+
+    assert_eq!(ast.len(), 1);
+
+    let parametric = PointMotionOperation::from_nodes(&ast).unwrap();
+
+    assert_eq!(correct, parametric.result);
+}
